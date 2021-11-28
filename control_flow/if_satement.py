0f0edf68110586e9c47e8874a676d6519bee6a9b
mayursangani <mayur.sangani@gmail.com>
2021-11-28 01:31:41 +0000

    if, elif and else statement

--- control_flow/if_satement.py
+++ control_flow/if_satement.py
@@ -1,7 +1,9 @@
 name = "Mayur"
-age = 46
+age = 20
 
-if age <= 43:
-    print("Sorry, you must be over the age of 43 to learn python")
+if age <= 14:
+    print("Sorry, you must be over the age of 18 to learn python")
+elif age < 18:
+    print("Users the under the age of 18 must ask mummy or daddy")
 else:
     print("Thank you, you're old enough")    

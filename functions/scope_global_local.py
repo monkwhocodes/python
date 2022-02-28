2c490ad3716d639afe86ea4bc30f03eb30b25128
mayursangani <mayur.sangani@gmail.com>
2022-02-28 22:00:10 +0000

    functions lessons added

---
+++ functions/scope_global_local.py
@@ -0,0 +1,9 @@
+message = 'Welcome'
+
+def output_greeting(name):
+    """Output a greeting for given name"""
+    message = f'Hello {name}'
+    print(message)
+    
+output_greeting('Mayur')
+print(message)
